b90cdbfc0ed4870122d7ae0cab85117507613643
mferos <101940463+mferos@users.noreply.github.com>
2022-03-27 13:30:47 +0530

    Add files via upload

---
+++ week2/day1/Login.java
@@ -0,0 +1,40 @@
+package week2.day1;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Login {
+	public static void main(String[] args) {
+		
+		WebDriverManager.chromedriver().setup();
+		
+		ChromeDriver driver=new ChromeDriver();
+		
+		driver.get("http://leaftaps.com/opentaps/control/main");
+		
+		driver.manage().window().maximize();
+		
+		driver.findElement(By.id("username")).sendKeys("DemoCSR");
+		driver.findElement(By.id("password")).sendKeys("crmsfa");
+		driver.findElement(By.className("decorativeSubmit")).click();
+		driver.findElement(By.linkText("CRM/SFA")).click();
+		driver.findElement(By.linkText("Leads"));
+		driver.findElement(By.linkText("Create Lead")).click();
+		driver.findElement(By.id("createLeadForm_companyName")).sendKeys("TestLeaf");
+		driver.findElement(By.id("createLeadForm_firstName")).sendKeys("Mohamed");
+		driver.findElement(By.id("createLeadForm_lastName")).sendKeys("Feros");
+				
+		WebElement w = driver.findElement(By.id("createLeadForm_dataSourceId"));
+		Select dd = new Select(w);
+		dd.selectByVisibleText("Employee");
+		
+		driver.findElement(By.name("submitButton")).click();
+		
+		//Thread.sleep(5000);
+		//driver.close();
+	}
+}
---
+++ week2/day1/assignments/DuplicateLead.java
@@ -0,0 +1,52 @@
+package week2.day1.assignments;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class DuplicateLead {
+
+	public static void main(String[] args) throws InterruptedException {
+		
+		WebDriverManager.chromedriver().setup();
+		ChromeDriver cd = new ChromeDriver();
+		cd.get("http://leaftaps.com/opentaps/control/login");
+		cd.manage().window().maximize();
+		
+		cd.findElement(By.id("username")).sendKeys("DemoCSR");
+		cd.findElement(By.id("password")).sendKeys("crmsfa");
+		cd.findElement(By.className("decorativeSubmit")).click();
+		cd.findElement(By.linkText("CRM/SFA")).click();
+		cd.findElement(By.linkText("Leads"));
+		cd.findElement(By.linkText("Create Lead")).click();
+		cd.findElement(By.id("createLeadForm_companyName")).sendKeys("TestLeaf");
+		cd.findElement(By.id("createLeadForm_firstName")).sendKeys("Mohamed");
+		cd.findElement(By.id("createLeadForm_lastName")).sendKeys("Feros");
+		cd.findElement(By.id("createLeadForm_firstNameLocal")).sendKeys("Feros");
+		cd.findElement(By.id("createLeadForm_departmentName")).sendKeys("IT");
+		cd.findElement(By.id("createLeadForm_description")).sendKeys("Selenium Assignment");
+		cd.findElement(By.id("createLeadForm_primaryEmail")).sendKeys("feroseh19@gmail.com");
+		
+		WebElement w = cd.findElement(By.id("createLeadForm_generalStateProvinceGeoId"));
+		Select s = new Select(w);
+		s.selectByVisibleText("New York");
+		
+		cd.findElement(By.name("submitButton")).click();
+		
+		Thread.sleep(2000);
+		
+		cd.findElement(By.linkText("Duplicate Lead")).click();
+		cd.findElement(By.id("createLeadForm_companyName")).clear();
+		cd.findElement(By.id("createLeadForm_companyName")).sendKeys("New TestLeaf");
+		cd.findElement(By.id("createLeadForm_firstName")).clear();
+		cd.findElement(By.id("createLeadForm_firstName")).sendKeys("M");
+
+		cd.findElement(By.name("submitButton")).click();
+		
+		System.out.println("Title - " + cd.getTitle());
+		
+	}
+}
